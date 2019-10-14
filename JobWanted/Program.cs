0ae48470c97b17885990f44e91b6743640866b8e
alien <734329218@qq.com>
2019-10-14 16:07:51 +0800

    Update Program.cs
    升级.net core 3.0

--- JobWanted/Program.cs
+++ JobWanted/Program.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace JobWanted
 {
@@ -12,21 +9,19 @@ namespace JobWanted
     {
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
-                  .SetBasePath(Directory.GetCurrentDirectory())
-                  .AddJsonFile("hosting.json", optional: true)
-                  .Build();
-
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseConfiguration(config)
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
-
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureServices(configureDelegate =>
+                    configureDelegate.AddApplicationInsightsTelemetry())
+                .ConfigureLogging(configureLogging =>
+                    configureLogging.AddConsole().AddDebug()
+                );
     }
 }
